6a2cab38ca16a68ad1f52ff2f65b9cdb13a72440
Kishorpatil007 <kp675541@gmail.com>
2026-02-04 11:57:57 +0530

    Add files via upload

---
+++ 6.PosiOrNega/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+            Console.WriteLine("\nThe number is Positive");
+        else if (num < 0)
+            Console.WriteLine("\nThe number is Negative");
+        else
+            Console.WriteLine("\nThe number is Zero");
+    }
+}
