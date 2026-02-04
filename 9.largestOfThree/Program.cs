320e258927eaa58e5b7382ddb36c83e020d713d6
Kishorpatil007 <kp675541@gmail.com>
2026-02-04 12:04:19 +0530

    Add files via upload

---
+++ 9.largestOfThree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int largest;
+
+        if (a >= b && a >= c)
+            largest = a;
+        else if (b >= a && b >= c)
+            largest = b;
+        else
+            largest = c;
+
+        Console.WriteLine("\nLargest number = " + largest);
+    }
+}
