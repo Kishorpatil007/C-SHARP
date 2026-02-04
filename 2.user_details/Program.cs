a8ae34c4130c1049b233d02be1ece70327ae189b
Kishorpatil007 <kp675541@gmail.com>
2026-02-04 11:50:20 +0530

    Add files via upload

---
+++ 2.user_details/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Class: ");
+        string userClass = Console.ReadLine();
+
+        Console.Write("Enter Roll No: ");
+        string rollNo = ConvertToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n--- Student Details ---");
+        Console.WriteLine("Name     : " + name);
+        Console.WriteLine("Class    : " + userClass);
+        Console.WriteLine("Roll No  : " + rollNo);
+    }
+}
