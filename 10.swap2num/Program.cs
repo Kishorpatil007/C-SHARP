e76ac20c7d2158f41df47fb335c3a4467400d444
Kishorpatil007 <kp675541@gmail.com>
2026-02-04 12:05:47 +0530

    Add files via upload

---
+++ 10.swap2num/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("\nAfter Swapping:");
+        Console.WriteLine("First number = " + a);
+        Console.WriteLine("Second number = " + b);
+    }
+}
+
